d601b1fcceddca20fc99689e6438f8b9e3ef5eff
Manuel Eduardo <eduardo-mce@hotmail.com>
2018-05-07 07:49:41 -0500

    metodos terminados para el examen parcial

--- metodos.py
+++ metodos.py
@@ -129,8 +129,61 @@ def metodoCrankNicolson(c, L, T, h, k, f, a, b):
     return sol
 
 
+def metodoHiperbolica(c, L, T, h, k, f, g, a, b):
+     
+    r = (c*k**2)/h**2
+    n = round(T/k) + 1
+    m = round(L/h) + 1
+
+    x = np.linspace(0,L,m)
+    
+    sol = np.zeros((n,m))
+
+    sol[0] = f(x)
+    #primera forma
+    #sol[1] = f(x) + k*g(x)
+
+    #segunda forma
+    for j in range(1,m-1):
+        sol[1,j] = (1-r)*f(x[j]) + (r/2)*f(x[j+1]) + (r/2)*f(x[j-1]) + k*g(x[j])
+
+    sol[:,0] = a(0)
+    sol[:,-1] = b(0)
+
+    A = tridiag(r, 2-2*r, r, m-2)
+
+    for i in range(1,n-1):
+        sol[i+1,1:-1] = np.dot(A, sol[i,1:-1]) - sol[i-1,1:-1]
+        sol[i+1,1] += r*sol[i,0]
+        sol[i+1,-2] += r*sol[i,-1]
+        
+    return sol
+
+
+
 if __name__ == "__main__" :
     
-	 #print(metodoCrankNicolson(1, 1, 0.5, 0.1, 0.01, lambda x: np.sin(np.pi*x), lambda x: 0, lambda x: 0 ) )
-     print(metodoImplicito(0.1, 1, 0.3, 0.2, 0.1, lambda x: 100, lambda x: 20,
-     lambda x: 40 ) )
+    sol1 = metodoHiperbolica(1, 1, 1, 0.25, 0.25, lambda x: np.sin(np.pi*x), lambda x: 0, lambda x: 0, lambda x:0)
+    sol2 = metodoHiperbolica(1, np.pi, 0.5, np.pi/10.0, 0.05, lambda x: np.sin(x), lambda x: 0, lambda x: 0, lambda x:0)
+    sol3 = metodoHiperbolica(1, np.pi, 0.5, np.pi/20.0, 0.1, lambda x: np.sin(x), lambda x: 0, lambda x: 0, lambda x:0)
+    sol4 = metodoHiperbolica(1, np.pi, 0.5, np.pi/20.0, 0.05, lambda x: np.sin(x), lambda x: 0, lambda x: 0, lambda x:0)
+    sol5 = metodoHiperbolica(1, 1, 0.3, 0.1, 0.1, lambda x: np.sin(2*np.pi*x), lambda x: 2*np.pi*np.sin(2*np.pi*x), lambda x: 0, lambda x:0)
+    #print(sol5)
+    #sol = metodoExplicito(1, 1, 0.5, 0.1, 0.01, lambda x: np.sin(np.pi*x), lambda x: 0, lambda x: 0 )
+    #print(sol[-1])
+    #print(metodoExplicitoMatriz(0.1, 1, 0.3, 0.2, 0.1, lambda x: 100, lambda x: 20,
+    #lambda x: 40 ) )
+           
+    
+
+
+
+
+
+
+
+
+
+
+
+
